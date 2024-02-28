5d06154961d97b5deac4269168d6361678a285e7
TJ Porter <tayler.porter@mowgcap.org>
2024-02-28 12:08:20 -0600

    Added bootstrap Added navigation bar Added basic page

--- html/index.php
+++ html/index.php
@@ -1,8 +1,14 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <title>Civil Air Patrol Emergency Managent System</title>
+        <link href="/bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="/style.css" rel="stylesheet">
     </head>
     <body>
-        Hello, World!
+        <?php require("nav.php"); ?>
+        <script src="/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
---
+++ html/nav.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg bg-body tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand">
+            <img src="/images/Wing.php" width="60" height="60" class="d-inline block align-text-top"/>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <?php /*
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Map</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/getMembers">Members</a>
+                </li>
+                */
+                ?>
+                <li class="nav-item">
+                    <a class=nav-link" href="/">Status Board</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
