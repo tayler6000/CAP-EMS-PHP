597633eb5920c1503409c38e5817adacb6b74922
TJ Porter <tayler.porter@mowgcap.org>
2024-03-02 12:42:17 -0600

    Added basic row iterator

--- html/index.php
+++ html/index.php
@@ -29,6 +29,29 @@ error_reporting(E_ALL);
                     <th>Checkin</th>
                     <th>Action</th>
                 </tr>
+                <?php
+                $conn = mysqli_connect("localhost", getenv("DB_USER"), getenv("DB_PASS"), getenv("DB_USER"));
+                $stmt = $conn->prepare("SELECT * FROM `deployed_ground`");
+                $stmt->execute();
+                $result = $stmt->get_result();
+                if($result === False){
+                    print($conn->error);
+                }
+                while $row = mysqli_fetch_assoc($result){
+                    print("<tr>");
+                    print("<th>".$row->sortie."</th>");
+                    print("<td>Name</td>");
+                    print("<td>COV</td>");
+                    print("<td>Driver</td>");
+                    print("<td>GTL</td>");
+                    print("<td>Passengers</td>");
+                    print("<td>Status</td>");
+                    print("<td>Location</td>");
+                    print("<td>Checkin</td>");
+                    print("<td>Action</td>");
+                    print("</tr>");
+                }
+                ?>
             </table>
         </div>
         <script src="/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
