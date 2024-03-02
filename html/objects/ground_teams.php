0222eb1c724eb2dd7f650d7b302c35e40ece6794
TJ Porter <tayler.porter@mowgcap.org>
2024-03-02 12:38:00 -0600

    Fixed issues with Ground Team object

--- html/objects/ground_teams.php
+++ html/objects/ground_teams.php
@@ -8,12 +8,17 @@
         public int $passengers;
         public string $status;
         public string $location;
-        public int $checkin;
+        public string $checkin;
+
         public function __construct(int $id){
             $conn = mysqli_connect("localhost", getenv("DB_USER"), getenv("DB_PASS"), getenv("DB_USER"));
             $stmt = $conn->prepare("SELECT * FROM `deployed_ground` WHERE `sortie`=?");
             $stmt->bind_param("i", $id);
-            $result = $stmt->execute();
+            $stmt->execute();
+            $result = $stmt->get_result();
+            if($result === False){
+                print($conn->error);
+            }
             $row = mysqli_fetch_assoc($result);
             $this->sortie = $row["sortie"];
             $this->name = $row["name"];
@@ -23,7 +28,7 @@
             $this->passengers = $row["passengers"];
             $this->status = $row["status"];
             $this->location = $row["location"];
-            $this->checkin = $ow["checkin"];
+            $this->checkin = $row["checkin"];
         }
     }
 ?>
