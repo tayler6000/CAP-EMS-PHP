f58c042b1bb08fd152e8c87952f9a4701556c3d6
TJ Porter <tayler.porter@mowgcap.org>
2024-03-03 16:14:29 -0600

    Added ajax update function to load table

--- html/index.php
+++ html/index.php
@@ -11,6 +11,7 @@
         <title>Civil Air Patrol Emergency Managent System</title>
         <link href="/bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link href="/style.css" rel="stylesheet">
+        <script src="/js/update.js"></script>
     </head>
     <body>
         <?php require("nav.php"); ?>
@@ -18,6 +19,9 @@
             <h6>Ground Teams</h6>
             <div id="ground_teams"></div>
         </div>
+        <script>
+            update("/api/ground_teams.php", "ground_teams");
+        </script>
         <script src="/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
