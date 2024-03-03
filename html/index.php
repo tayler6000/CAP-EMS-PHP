918fdc8bcc385b1b48abdfe2cd9be034cc674622
TJ Porter <tayler.porter@mowgcap.org>
2024-03-03 16:01:15 -0600

    Moved ground teams table to api folder

--- html/index.php
+++ html/index.php
@@ -1,8 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-require_once("objects/ground_teams.php");
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,44 +16,7 @@ require_once("objects/ground_teams.php");
         <?php require("nav.php"); ?>
         <div class="container">
             <h6>Ground Teams</h6>
-            <table style="text-align:center;">
-                <tr>
-                    <th>Sortie #</th>
-                    <th>Name</th>
-                    <th>COV</th>
-                    <th>Driver</th>
-                    <th>GTL</th>
-                    <th>Passengers</th>
-                    <th>Status</th>
-                    <th>Location</th>
-                    <th>Checkin</th>
-                    <th>Action</th>
-                </tr>
-                <?php
-                $conn = mysqli_connect("localhost", getenv("DB_USER"), getenv("DB_PASS"), getenv("DB_USER"));
-                $stmt = $conn->prepare("SELECT * FROM `deployed_ground`");
-                $stmt->execute();
-                $result = $stmt->get_result();
-                if($result === False){
-                    print($conn->error);
-                }
-                while($row = mysqli_fetch_assoc($result)){
-                    $team = new GroundTeam($row["sortie"]);
-                    print("<tr>");
-                    print("<td>".$team->sortie."</td>");
-                    print("<td>".$team->name."</td>");
-                    print("<td>".$team->cov."</td>");
-                    print("<td>".$team->driver."</td>");
-                    print("<td>".$team->leader."</td>");
-                    print("<td>".$team->passengers."</td>");
-                    print("<td>".$team->status."</td>");
-                    print("<td>".$team->location."</td>");
-                    print("<td>".$team->checkin."</td>");
-                    print("<td><button>Action</button></td>");
-                    print("</tr>");
-                }
-                ?>
-            </table>
+            <div id="ground_teams"></div>
         </div>
         <script src="/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
