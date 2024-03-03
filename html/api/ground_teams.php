1caa44864a9926d1e6477771428403daa9ae9dc8
TJ Porter <tayler.porter@mowgcap.org>
2024-03-03 17:55:47 -0600

    Now will go orange at 30 minutes and red at 35.

--- html/api/ground_teams.php
+++ html/api/ground_teams.php
@@ -3,7 +3,8 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
     require_once("../objects/ground_teams.php");
-    $late_offset = 30 * 60;
+    $late_offset = 35 * 60;
+    $warning_offset = 30 * 60;
 ?>
 <table style="text-align:center;">
     <tr>
@@ -31,6 +32,8 @@
             print("<tr");
             if((time() - $late_offset) > $team->checkin){
                 print(" style='background-color: red;color: white;'");
+            }elseif((time() - $warning_offset) > $team->checkin){
+                print(" style='background-color: orange;color: white;'");
             }
             print(">");
             print("<td>".$team->sortie."</td>");
