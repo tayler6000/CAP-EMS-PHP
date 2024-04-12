673b94b4ab5fc3983028523b23f9b82ab889e3f0
TJ Porter <tayler.porter@mowgcap.org>
2024-04-12 17:05:42 -0500

    Added link to repo on copyright notice

--- html/copyright.php
+++ html/copyright.php
@@ -1,2 +1,2 @@
-<center><span>&copy; 2024 2d Lt TJ Porter</span></center>
+<center><span><a style="color:black;text-decoration:none;" href="https://github.com/tayler6000/CAP-EMS-PHP/tree/master">&copy; 2024 2d Lt TJ Porter</a></span></center>
 <?php include("version.php"); ?>
