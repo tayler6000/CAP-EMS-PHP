7525ed89e3f81fb260d53ca59b99540555693d63
TJ Porter <tayler.porter@mowgcap.org>
2024-04-12 20:20:46 -0500

    Fix version checking Change copyright to open repo in new tab

--- html/copyright.php
+++ html/copyright.php
@@ -1,2 +1,2 @@
-<center><span><a style="color:black;text-decoration:none;" href="https://github.com/tayler6000/CAP-EMS-PHP/tree/master">&copy; 2024 2d Lt TJ Porter</a></span></center>
+<center><span><a target="_blank" style="color:black;text-decoration:none;" href="https://github.com/tayler6000/CAP-EMS-PHP/tree/master">&copy; 2024 2d Lt TJ Porter</a></span></center>
 <?php include("version.php"); ?>
--- html/version.php
+++ html/version.php
@@ -1,17 +1,19 @@
 <?php
 
 $version = "1.0.0-beta";
-$semver_regex = "^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$";
+$semver_regex = "/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/";
 $ch = curl_init("https://api.github.com/repos/tayler6000/CAP-EMS-PHP/releases/latest");
+$headers = ["User-Agent: CAP EMS Server"];
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 $result = json_decode(curl_exec($ch), true);
 curl_close($ch);
 
 if(isset($result["tag_name"])){
     $current_version = $result["tag_name"];
     if($current_version !== $version){
-        $current_version_match = preg_match($semver_regex, $current_version);
-        $version_match = preg_match($semver_regex, $version);
+        preg_match($semver_regex, $current_version, $current_version_match);
+        preg_match($semver_regex, $version, $version_match);
         $current_major = (int)$current_version_match[1];
         $current_minor = (int)$current_version_match[2];
         $current_patch = (int)$current_version_match[3];
